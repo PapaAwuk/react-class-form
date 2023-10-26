6a69d9589f9f1a13dbe1b1344f4e03008f3c8708
Albert <albertawukuyoung@gmail.com>
2023-10-26 17:16:52 +0000

    React-Forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import ClassForm from "./components/ClassForm";
-import FunctionalForm from "./components/FunctionalForm";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <ClassForm />
-      <br /> <br />
-      <FunctionalForm />
-    </div>
-  );
+class App extends Component() {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+    };
+  }
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleNameChange}
+          value={this.state.name}
+          t
+          ype="text"
+          placeholder="Enter your name"
+        />
+        <input
+          onChange={this.handleEmailChange}
+          value={this.state.email}
+          type="email"
+          placeholder="Enter your Email"
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
 }
-
 export default App;
